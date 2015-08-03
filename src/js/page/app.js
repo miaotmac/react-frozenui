fb21b32defff862c4b44c8f943846675c4880bb8
miaotmac <miaotmac@126.com>
2015-08-03 08:59:00 +0800

    add rhl config

--- src/js/page/app.js
+++ src/js/page/app.js
@@ -2,8 +2,9 @@ var React = require('react'),
     SearchBar = require('../component/SearchBar'),
     List = require('../component/List/List'),
     ListItem = require('../component/List/ListItem'),
-    PureList = require('../component/List/PureList');
-
+    PureList = require('../component/List/PureList')
+    ,Dialog = require('../component/Dialog')
+    ;
 var wrap = document.querySelector('.wrap');
 
 var ListInstance = (
@@ -12,11 +13,11 @@ var ListInstance = (
             <List fzStyle="pure">
                 <ListItem>
                      <p><span>1.faycheng </span><span class="date"> 2月12日</span></p>
-                     <h4>这本书太赞了，每次看都有不一样的体会和感悟，超级喜欢！期待大结局。</h4>
+                     <h4>这本书太赞了dddd，每次看都有不一样的体会和感悟，超级喜欢！期待大结局。</h4>
                 </ListItem>
                 <ListItem>
                         <p><span>2.faycheng </span><span class="date"> 2月12日</span></p>
-                         <h4>标题标题标题标题标题标题题标题标题标题标题标题标题标题标题标题</h4>
+                         <h4>标题标题标题标题ccccc标题标题题标题标题标题标题ddddd标题标题标题标题标题</h4>
                 </ListItem>
             </List>
         </section>
@@ -24,8 +25,8 @@ var ListInstance = (
 );
 
 var data = [
-	{title:'1.faycheng',date:'2月12日',content:'这本书太赞了，每次看都有不一样的体会和感悟，超级喜欢！期待大结局。'},
-	{title:'2.faycheng',date:'2月12日',content:'这本书太赞了，每次看都有不一样的体会和感悟，超级喜欢！期待大结局。'}
+	{title:'1.faycheng',date:'2月12日',content:'这本书太赞了，每次看都有不ssss一样的体会和感悟，超级喜欢！期待大结局。'},
+	{title:'2.faycheng',date:'2月12日',content:'这本书太赞了，每次看都有不一样的sdfsdfsd体会和感悟，超级喜欢！期待大结局。'}
 ]
 var pureListInstance = (
   <div>
@@ -39,7 +40,18 @@ var buttonsInstance = (
   <SearchBar searchText="qingshuru" placeholder="qingshuru" type="num" cancelText="qqxiao"/>
 );
 
+var handleDialogClick = function() {
+  this.refs.ddialog.setState({show:true});
+};
+
+var dialogInstance = (
+    <div>  
+    <button type= "button" onclick = "handleDialogClick()" >clicqqkddd</button>
+    <Dialog ref = "ddialog" content ="内容" title = "标题" />
+    </div>
+  );
+
 React.render(
-    pureListInstance, wrap
+    dialogInstance, wrap
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
 
     var webpack = require('webpack');
+    var path = require('path');
     var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
     //var ExtractTextPlugin = require("extract-text-webpack-plugin");
     var entry = require('./index.js');
     module.exports = {
         //plugins: [commonsPlugin, new ExtractTextPlugin("[name].css")],
-        plugins: [commonsPlugin],
-        entry: entry,
+       // plugins: [commonsPlugin],
+        plugins: [
+             new webpack.DefinePlugin({
+                'process.env.NODE_ENV': 'JSON.stringify("development")'
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoErrorsPlugin(),
+            commonsPlugin
+        ],
+
+        entry : entry
+       /* entry: [
+              //  entry
+              'webpack-dev-server/client?http://127.0.0.1:3000',// WebpackDevServer host and port
+              'webpack/hot/only-dev-server',
+              './src/js/page/app.js',
+              './src/js/component/tag.js',
+              './index.js'
+               ]
+        */
+        ,
         output: {
-            path: 'dist/js/page',
-            filename: '[name].js'
+            path: path.join(__dirname, 'dist/js/'),
+            filename: '[name].js',
+            publicPath: '/dist/'
         },
 
         module: {
             loaders: [
                 { test: /\.css$/, loader: 'style-loader!css-loader' },
-                { test: /\.js$/, loader: 'jsx-loader?harmony' },
+                { test: /\.js$/, loaders: ['jsx-loader?harmony','react-hot'] },
                 { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
                 { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
-                { test: /\.jsx?$/,exclude: /(node_modules|bower_components)/,loader: 'babel'}
+                { test: /\.jsx?$/,exclude: /(node_modules|bower_components)/,include: path.join(__dirname, 'src'),loaders: ['babel','react-hot']}
             ]
         },
         resolve: {
